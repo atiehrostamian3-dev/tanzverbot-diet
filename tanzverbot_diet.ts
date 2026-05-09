3d6159b71d7687699bdec5dd257c96e6d4b792a2
Atieh_Ros <atieh_rostamian@sluz.ch>
2026-05-09 12:30:05 +0200

    Final: Make food list injectable (Erweiterung 2)

--- tanzverbot_diet.ts
+++ tanzverbot_diet.ts
@@ -5,13 +5,15 @@ export enum Sex {
 
 const CALORIES_PER_KG_FAT = 9000;
 
-interface FoodItem {
+//Die Struktur für Lebensmittel definieren
+export interface FoodItem {
   name: string;
   calories: number;
   servings: number;
 }
 
-const dietFoods: FoodItem[] = [
+//Standardliste der Tans-Verboot-Diät
+export const dietFoods: FoodItem[] = [
   { name: "Kellogg's Tresor", calories: 137, servings: 4 },
   { name: "Weihenstephan Haltbare Milch", calories: 64, servings: 8 },
   { name: "Mühle Frikadellen", calories: 271, servings: 4 },
@@ -25,6 +27,7 @@ const dietFoods: FoodItem[] = [
 ];
 
 export function calcDateOnDiet(
+  foodItems: FoodItem[], //Die Liste der Lebensmittel wird nun als Eingabe akzeptiert.
   currentWeightKg: number,
   targetWeightKg: number,
   heightM: number,
@@ -40,13 +43,14 @@ export function calcDateOnDiet(
   }
   
   let dailyCaloriesOnDiet = 0;
-  for (const food of dietFoods) {
+  //Nun verwenden wir die Liste, die an die Funktion übergeben wurde.
+  for (const food of foodItems) {
     dailyCaloriesOnDiet += food.calories * food.servings;
   }
 
   const averageWeightKg = (currentWeightKg + targetWeightKg) / 2;
   const dailyCaloriesBasicMetabolicRate = calcBMR(averageWeightKg, heightM, ageY, sex);
-  
+
   const dailyExcessCalories =
     dailyCaloriesOnDiet - dailyCaloriesBasicMetabolicRate;
   if (dailyExcessCalories <= 0) {
@@ -58,14 +62,8 @@ export function calcDateOnDiet(
 
 function calcBMR(weightKg: number, heightM: number, ageY: number, sex: Sex): number {
   if (sex == Sex.Male) {
-    return Math.ceil(
-      // Harris-Benedict-Formula (Male)
-      66.47 + 13.7 * weightKg + 5.003 * heightM * 100.0 - 6.75 * ageY,
-    );
+    return Math.ceil(66.47 + 13.7 * weightKg + 5.003 * heightM * 100.0 - 6.75 * ageY);
   } else {
-    return Math.ceil(
-      // Harris-Benedict-Formula (Female)
-      655.1 + 9.563 * weightKg + 1.85 * heightM * 100.0 - 4.676 * ageY,
-    );
+    return Math.ceil(655.1 + 9.563 * weightKg + 1.85 * heightM * 100.0 - 4.676 * ageY);
   }
 }
--- tanzverbot_diet_test.ts
+++ tanzverbot_diet_test.ts
@@ -1,7 +1,12 @@
 import { assertGreater } from "@std/assert";
-import { calcDateOnDiet, Sex } from "./tanzverbot_diet.ts";
+import { calcDateOnDiet, Sex, dietFoods } from "./tanzverbot_diet.ts";
+
+Deno.test("Tanzverbot Diet - Original Test", () => {
+//Wir haben die Liste „dietFoods“ als ersten Parameter hinzugefügt.
+  assertGreater(calcDateOnDiet(dietFoods, 74, 100, 1.86, 38, Sex.Male), 0.0);
+});
 
 Deno.test("Tanzverbot Diet - Custom Test for Female", () => {
-  const days = calcDateOnDiet(60, 70, 1.70, 30, Sex.Female);
+  const days = calcDateOnDiet(dietFoods, 60, 70, 1.70, 30, Sex.Female);
   assertGreater(days, 0.0);
 });
